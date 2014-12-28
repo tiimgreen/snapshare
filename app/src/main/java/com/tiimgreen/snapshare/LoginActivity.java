f40d1a9a9cbb9d546d0e1a714fa5f1374bafa1da
Tim Green <tiimgreen@gmail.com>
2014-12-28 02:07:01 +0000

    Link Sign In/Up pages

--- app/src/main/java/com/tiimgreen/snapshare/LoginActivity.java
+++ app/src/main/java/com/tiimgreen/snapshare/LoginActivity.java
@@ -1,19 +1,32 @@
 package com.tiimgreen.snapshare;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class LoginActivity extends ActionBarActivity {
+
+    protected TextView mSignUpTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
+        mSignUpTextView = (TextView) findViewById(R.id.signUpTextView);
+        mSignUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/tiimgreen/snapshare/SignUpActivity.java
+++ app/src/main/java/com/tiimgreen/snapshare/SignUpActivity.java
@@ -1,17 +1,31 @@
 package com.tiimgreen.snapshare;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class SignUpActivity extends ActionBarActivity {
+
+    protected TextView mSignInTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mSignInTextView = (TextView) findViewById(R.id.signInTextView);
+        mSignInTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
